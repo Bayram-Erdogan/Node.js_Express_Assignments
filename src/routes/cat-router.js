b251040be33ac2cf82f75db5b0cc17948d2549d8
Bayram-Erdogan <143434001+Bayram-Erdogan@users.noreply.github.com>
2026-04-04 09:17:42 +0300

    Branch Assignmet4 has been added

--- src/routes/cat-router.js
+++ src/routes/cat-router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-
+import path from 'path';
 import {
   getAllCats,
   getCatById,
@@ -8,14 +8,24 @@ import {
   updateCat,
   deleteCat,
 } from '../controllers/cat-controller.js';
+import {createThumbnail} from '../middlewares/upload.js';
 
 const router = express.Router();
 
-const upload = multer({dest: 'uploads/'});
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) + ext;
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({storage});
 
 router.get('/', getAllCats);
 router.get('/:id', getCatById);
-router.post('/', upload.single('cat'), createCat);
+router.post('/', upload.single('cat'), createThumbnail, createCat);
 router.put('/:id', updateCat);
 router.delete('/:id', deleteCat);
 
